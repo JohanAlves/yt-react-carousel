b4289090bcf4fea3c4eed9aaba93fd74ff502d01
Johan Alves <johanvaf@gmail.com>
2022-07-14 18:47:59 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Carousel from "./components/Carousel";
+import { countries } from "./components/Data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topContent">
+        <h2>Places to Meet</h2>
+        <p>After you Like &amp; Subscribe!</p>
+      </div>
+      {/* Carousel */}
+      <Carousel images={countries} />
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,22 @@
+export const countries = [
+  {
+    image: "/images/brazil.jpg",
+    title: "Brazil",
+  },
+  {
+    image: "/images/china.jpg",
+    title: "China",
+  },
+  {
+    image: "/images/france.jpg",
+    title: "France",
+  },
+  {
+    image: "/images/japan.jpg",
+    title: "Japan",
+  },
+  {
+    image: "/images/norway.jpg",
+    title: "Norway",
+  },
+];
